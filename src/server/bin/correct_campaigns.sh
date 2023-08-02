4c8c101384d1ff127b4647b1d0c6714621d48fc2
Tharles de Sousa Andrade <irtharles@gmail.com>
2023-08-01 22:19:51 -0300

    update correct campaigns

--- src/server/bin/correct_campaigns.sh
+++ src/server/bin/correct_campaigns.sh
@@ -2,7 +2,7 @@
 
 while read -r campaign; do
 #  /APP/tvi/src/server/bin/correct_campaigns.log
-  echo "$(date) - Calling: $campaign" >> ./correct_campaigns.log
+  echo "$(date) - Calling: $campaign" >> /APP/tvi/src/server/bin/correct_campaigns.log
 #  mongo tvi-indonesia --host 172.18.0.6 --eval 'var campaignId="'$campaign'";' correct_campaign.js > /dev/null 2>&1
-  mongo tvi-indonesia --host 172.18.0.6 --eval 'var campaignId="'$campaign'";' correct_campaign.js
+  mongo tvi-indonesia --host 172.18.0.6 --eval 'var campaignId="'$campaign'";' correct_campaign.js >> /APP/tvi/src/server/bin/correct_campaigns.log
 done < "$1"
