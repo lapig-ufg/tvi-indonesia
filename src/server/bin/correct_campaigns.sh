ad8ffe42c841d91366aecdf167501492e5e15f8e
Tharles de Sousa Andrade <irtharles@gmail.com>
2023-08-01 21:53:36 -0300

    change htto verb

--- src/server/bin/correct_campaigns.sh
+++ src/server/bin/correct_campaigns.sh
@@ -3,8 +3,8 @@
 url_base="https://tvi-indonesia.lapig.iesa.ufg.br/service/campaign/correct?campaignId="
 
 while read -r campaign; do
-  url="${url_base}${campaign}"
-  echo  "$url"
-  echo "$(date) - Calling: $url" >> /APP/tvi/src/server/bin/correct_campaigns.log
-  curl "$url" > /dev/null 2>&1
+#  url="${url_base}${campaign}"
+#  echo  "$url"
+#  echo "$(date) - Calling: $url" >> /APP/tvi/src/server/bin/correct_campaigns.log
+#  curl "$url" > /dev/null 2>&1
 done < "$1"
